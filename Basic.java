c9aa46fe922b5da7ce9508cd37d9d7ac390bf17c
Mohit-sd17 <72434244+Mohit-sd17@users.noreply.github.com>
2021-01-13 10:02:10 +0530

    Basic structure of java

---
+++ Basic.java
@@ -0,0 +1,17 @@
+// importing is manditory
+package JAVA;
+
+// Always declare the main class as public
+public class Basic {
+
+    // Declaring pulic static main method
+    // with return type void
+    // and which taske arguments of string array type
+    public static void main(String[] args) {
+
+        // For printing statement
+        // using print / println method from out stream of system class
+        // which takes argument of string type
+        System.out.println("This Statement will be printed on console!!");
+    }
+}
